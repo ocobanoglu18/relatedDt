a92318ab24fa07fb3ffd3768530d98bef1cdef4f
Deniz Kocaboğa <denizkocaboga@gmail.com>
2022-02-18 14:41:34 +0300

    Update EmailUtilities.cs
    1. _isValid'i içeri al.
    2. MailAdress dışında daha uygun bir yol var mı diye araştır. Daha uygun yol performans, doğru sonuç üretmesi ve kod sadeliği açısından değerlendirilecek.

--- userEmailDomainInfo/EmailUtilities.cs
+++ userEmailDomainInfo/EmailUtilities.cs
@@ -10,7 +10,10 @@ public interface IEmailUtilites
 public class EmailUtilites : IEmailUtilites
 {
     private readonly IDomainParser _domainParser;
-    private bool _isValid;
+    private bool _isValid;//Bu niye burada? _isValid'in scopu sadece GetEmailDomainName kadar. Yani _isValid'i instance'ın yaşamı boyunca kullanacak başka metod yok ve olmaz da. Metod'a verilen Email'in scopu kadardır bunun da scope'u.
+    
+    //Kullanıcının bu sınıfı kullanabilmesi için IDomainParser üretmesini zorunlu kılıyoruz. Kullanıcıyı bununla uğraşmaya zorlamasak iyi olur.
+    //Kullanımı kolaylaştırmak için ikinci bir parametresiz constructor oluşturup burada IDomainParser'ı default değerlerle biz oluşturabiliriz.    
     public EmailUtilites(IDomainParser domainParser)
     {
         //DomainParser'ı create etmek çok maliyetli olduğu için instance'da bir defa create edip sonra hep aynı instance'ı kullanıyoruz!
@@ -20,6 +23,9 @@ public class EmailUtilites : IEmailUtilites
     public string GetEmailDomainName(string email, bool includeSubdomain = false)
     {
         string? emailDomainName = null;
+        //Email validasyonu için MailAddress kullanma maliyeti nedir? 
+        //Email validasyonu için tonla yöntem var. Regex vs.
+        //ToDo: Başka bir yöntem bulmalı. Diğer yöntemlerle karşılaştırıp en performanslı çözüm üretililmeli.
         _isValid = MailAddress.TryCreate(email, result: out MailAddress addr);
 
         if (_isValid)
@@ -27,7 +33,7 @@ public class EmailUtilites : IEmailUtilites
             DomainInfo? domainInfo = _domainParser.Parse(addr.Host);
             if (domainInfo != null)
             {
-                if (includeSubdomain && domainInfo.SubDomain != null)
+                if (includeSubdomain && domainInfo.SubDomain != null)//IsNullOrEmpty daha doğru olur. 
                 {
                     emailDomainName = $"{domainInfo.SubDomain}.{domainInfo.Domain}";
                 }
